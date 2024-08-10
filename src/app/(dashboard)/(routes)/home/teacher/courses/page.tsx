ca742db7a4c4cce7a510420df216b161195e64a5
Luiz Victor Teodoro Mendonça de Carvalho <luizvictortmc@gmail.com>
2024-08-10 16:24:47 -0300

    teacher mode started

---
+++ src/app/(dashboard)/(routes)/home/teacher/courses/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const CoursesPage = () => {
+    return ( 
+        <div className="p-6">
+            <Link href="/home/teacher/create">
+                <Button>
+                    Novo Curso
+                </Button>
+            </Link>
+        </div>
+     );
+}
+ 
+export default CoursesPage;
--- src/components/navbar-routes.tsx
+++ src/components/navbar-routes.tsx
@@ -1,12 +1,37 @@
 "use client"
 
 import { UserButton } from "@clerk/nextjs"
+import { usePathname } from "next/navigation"
+import { Button } from "./ui/button";
+import { LogOut } from "lucide-react";
+import Link from "next/link";
 
 export const NavbarRoutes = () => {
 
+    const pathname = usePathname();
+
+    const isTeacherPage = pathname?.startsWith("/home/teacher");
+    const isPlayerPage = pathname?.includes("/home/chapter");
+
+
+
     return (
         <div className="flex gap-x-2 ml-auto">
-            <UserButton/>
+            {isTeacherPage || isPlayerPage ? (
+                <Link href="/home">
+                    <Button size="sm" variant="ghost">
+                        <LogOut className="h-4 w-4 mr-2"/>
+                        Exit
+                    </Button>
+                </Link>
+            ) : (
+                <Link href="/home/teacher/courses">
+                    <Button size="sm" variant="ghost">
+                        Teacher Mode
+                    </Button>
+                </Link>
+            )}
+            <UserButton />
         </div>
     )
 }
